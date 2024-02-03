e53b2402d13d6aa711b0b012301222db054ba44e
Bawergan <103044373+Bawergan@users.noreply.github.com>
2024-02-03 23:26:52 +0300

    cumit

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ struct Entity<'a> {
 
 impl Entity<'_> {
     fn recieve_damage(&mut self, dmg_amount: i32) {
-        self.health -= dmg_amount - self.armor;
+        self.health -= dmg_amount;
     }
 
     fn set_in_hand(&mut self, item: &Item) {
@@ -25,6 +25,7 @@ impl Entity<'_> {
 struct Item {
     name: String,
     damage: i32,
+    heal: i32,
 }
 impl PartialEq for Item {
     fn eq(&self, other: &Self) -> bool {
@@ -36,6 +37,12 @@ fn main() {
     let sword = Item {
         name: String::from("sword"),
         damage: 5,
+        heal: 0,
+    };
+    let potion = Item {
+        name: String::from("potion"),
+        damage: 0,
+        heal: 5,
     };
 
     let mut warior1 = Entity {
@@ -52,12 +59,12 @@ fn main() {
         health: 20,
         strength: 5,
         armor: 2,
-        inventory: [Some(&sword), None, None, None, None],
+        inventory: [Some(&potion), None, None, None, None],
         hand: None,
     };
 
     warior1.set_in_hand(&sword);
-    warior2.set_in_hand(&sword);
+    warior2.set_in_hand(&potion);
 
     let mut move_counter: u64 = 0;
     while warior1.health > 0 && warior2.health > 0 {
@@ -65,18 +72,41 @@ fn main() {
         println!("move: {}", move_counter);
 
         if move_counter % 2 == 0 {
-            match warior2.hand {
-                Some(hand) => warior1.recieve_damage(warior2.strength * hand.damage),
-                None => warior1.recieve_damage(warior2.strength),
-            }
+            do_move(&mut warior1, &mut warior2);
         } else {
-            match warior1.hand {
-                Some(hand) => warior2.recieve_damage(warior1.strength * hand.damage),
-                None => warior2.recieve_damage(warior1.strength),
-            }
+            do_move(&mut warior2, &mut warior1);
         }
 
         println!("{} health {}", warior1.name, warior1.health);
         println!("{} health {}", warior2.name, warior2.health);
     }
+}
+
+fn do_move(player1: &mut Entity, player2: &mut Entity) {
+    let mut dmg_amount = player1.strength / player2.armor;
+    let mut heal_amount = 1;
+
+    match player1.hand {
+        Some(hand) => {
+            dmg_amount *= hand.damage;
+            heal_amount *= hand.heal;
+        }
+        None => (),
+    }
+
+    player2.recieve_damage(dmg_amount);
+    player1.recieve_damage(-heal_amount);
+
+    println!(
+        "{} -> {}: {} dmg",
+        player1.name,
+        player2.name,
+        dmg_amount,
+    );
+    println!(
+        "{} -> {}: {} heal",
+        player1.name,
+        player1.name,
+        heal_amount,
+    );
 }
